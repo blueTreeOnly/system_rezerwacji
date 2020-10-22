7c9c0b8ee688748b35e20cc764a5fb1c786e7b3c
jl1977 <kuba.lasek@gmail.com>
2020-10-22 17:43:19 +0200

    Person.java added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        String hotelName = "Overlook";
+        int systemVersion = 1;
+        boolean isDeveloperVersion = true;
+
+        System.out.print("Witam w systemie rezerwacji dla hotelu");
+        System.out.println(hotelName);
+        System.out.println("Aktualna wersja systemu: ");
+        System.out.println(systemVersion);
+        System.out.println("Wersja developerska: ");
+        System.out.println(isDeveloperVersion);
+
+        System.out.println("\n=========================\n");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("1. Dodaj nowego gościa.");
+        System.out.println("2. Dodaj nowy pokój.");
+        System.out.println("Wybierz opcję: ");
+
+        int option = input.nextInt();
+
+        if(option == 1){
+            System.out.println("Wybrano opcję 1.");
+        }else{
+            System.out.println("Wybrano opcje 2. ");
+        }
     }
 }
