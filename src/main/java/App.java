85c6d8e61bcc0f7a4e7b36273047d13e4549d9a8
jl1977 <kuba.lasek@gmail.com>
2020-11-09 12:33:43 +0100

    Excercise 8

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -50,6 +50,14 @@ public class App {
             String lastName = input.next();
             System.out.println("Podaj wiek: ");
             int age = input.nextInt();
+            System.out.println("Podaj płeć (1. Mężczyzna, 2. Kobieta");
+            int genderOption = input.nextInt();
+            Gender gender = Gender.FEMALE;
+            if (genderOption == 1) {
+                gender = Gender.MALE;
+            } else if (genderOption == 2) {
+                gender = Gender.FEMALE;
+            }
             Guest newGuest = new Guest(firstName, lastName, age, gender);
             System.out.println(newGuest.getInfo());
             return newGuest;
--- src/main/java/Guest.java
+++ src/main/java/Guest.java
@@ -1,22 +1,15 @@
-import java.awt.*;
-
 public class Guest {
-
     private String firstName;
     private String lastName;
     private int age;
     private Gender gender;
-
-
     public Guest(String firstName, String lastName, int age, Gender gender) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.gender = gender;
-
     }
-
-    public String getInfo(){
+    public String getInfo() {
         return String.format("Dodano nowego gościa: %s %s (%d) ", this.firstName, this.lastName, this.age);
     }
 }
